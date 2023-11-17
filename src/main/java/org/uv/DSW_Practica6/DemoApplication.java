a4e22281ead03a96029aadf4062918a5ffe31c76
lilliamwitham <zs20006765@estudiantes.uv.mx>
2023-11-17 08:59:17 -0600

    Commit lili #1

--- src/main/java/org/uv/DSW_Practica6/DemoApplication.java
+++ src/main/java/org/uv/DSW_Practica6/DemoApplication.java
@@ -2,7 +2,7 @@ package org.uv.DSW_Practica6;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//commit
 @SpringBootApplication
 public class DemoApplication {
 
